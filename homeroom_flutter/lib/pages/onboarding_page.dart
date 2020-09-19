4febe316218f92d9899ec9d461c3b10cb9c630ab
Bharat Kathi <bharat1031@gmail.com>
2020-09-18 19:38:07 -0700

    Firebase boilerplate

---
+++ homeroom_flutter/lib/pages/onboarding_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:homeroom_flutter/utils/theme.dart';
+
+class OnboardingPage extends StatefulWidget {
+  @override
+  _OnboardingPageState createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: currBackgroundColor,
+      body: new Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              new Text(
+                "Welcome to Homeroom",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 60, color: currTextColor),
+              ),
+              new RaisedButton(
+                elevation: 0,
+                padding: EdgeInsets.only(left: 16, top: 8, bottom: 8, right: 16),
+                child: new Text("GET STARTED", style: TextStyle(fontSize: 35),),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(12))),
+                textColor: Colors.white,
+                color: mainColor,
+                onPressed: () {
+
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ homeroom_flutter/lib/utils/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+// MAIN THEME COLOR
+Color mainColor = const Color(0xFFffaa2b);
+
+// LIGHT THEME
+const lightTextColor = Colors.black;
+const lightBackgroundColor = Color(0xFFf9f9f9);
+const lightCardColor = Colors.white;
+const lightDividerColor = const Color(0xFFC9C9C9);
+
+// DARK THEME
+const darkTextColor = Colors.white;
+const darkBackgroundColor = const Color(0xFF212121);
+const darkCardColor = const Color(0xFF2C2C2C);
+const darkDividerColor = const Color(0xFF616161);
+
+// CURRENT COLORs
+var currTextColor = lightTextColor;
+var currBackgroundColor = lightBackgroundColor;
+var currCardColor = lightCardColor;
+var currDividerColor = lightDividerColor;
+
+final mainTheme = new ThemeData(
+    primaryColor: mainColor,
+    accentColor: mainColor,
+    fontFamily: "Product Sans",
+);
