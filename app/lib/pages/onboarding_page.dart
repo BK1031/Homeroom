0783e3e67c5e4fc2536d31b1bc3893755263f7a6
Bharat Kathi <bharat1031@gmail.com>
2020-09-20 07:09:01 -0700

    update landing page

--- app/lib/pages/onboarding_page.dart
+++ app/lib/pages/onboarding_page.dart
@@ -1,3 +1,4 @@
+import 'package:easy_web_view/easy_web_view.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:homeroom_flutter/utils/config.dart';
@@ -9,6 +10,9 @@ class OnboardingPage extends StatefulWidget {
 }
 
 class _OnboardingPageState extends State<OnboardingPage> {
+
+  static ValueKey key = ValueKey('key_0');
+  String src = "https://www.youtube.com/embed/ptBeQJm_xps";
 
   @override
   Widget build(BuildContext context) {
@@ -54,6 +58,49 @@ class _OnboardingPageState extends State<OnboardingPage> {
                     new Text(
                       "Welcome to Homeroom",
                       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 60, color: currTextColor),
+                    ),
+                    new Padding(padding: EdgeInsets.all(20)),
+                    new ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: new Container(
+                        width: (MediaQuery.of(context).size.width > 1300) ? 1100 : MediaQuery.of(context).size.width - 50,
+                        height: 550,
+                        color: Colors.black,
+                        child: Stack(
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                    flex: 1,
+                                    child: EasyWebView(
+                                        src: src,
+                                        onLoaded: () {
+                                          print('$key: Loaded: $src');
+                                        },
+                                        key: key
+                                    )
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    new Padding(padding: EdgeInsets.all(32)),
+                    Container(
+                      width: (MediaQuery.of(context).size.width > 1300) ? 1100 : MediaQuery.of(context).size.width - 50,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          new Text("We have noticed a larger lack of attention and interaction in online classes than in in-person classes.", style: TextStyle(color: mainColor, fontWeight: FontWeight.bold, fontSize: 40),),
+                          new Padding(padding: EdgeInsets.all(8)),
+                          new Text("This is why we created Homeroom, a more fun, interactive meeting software for education that caters to all grade levels. As part of HomeRoom, you can move an avatar around a virtual classroom, meeting other students. The audio projected to others scales based on your distance from them in the classroom, allowing you to have better peer-to-peer interactions.", style: TextStyle(color: currTextColor, fontWeight: FontWeight.normal, fontSize: 20),),
+                          new Padding(padding: EdgeInsets.all(16)),
+                          new Text("Break out into different table group discussions", style: TextStyle(color: mainColor, fontWeight: FontWeight.bold, fontSize: 40),),
+                          new Padding(padding: EdgeInsets.all(8)),
+                          new Text("Homeroom allows you to break out into different table group discussions and ask your teacher for one-on-one help by walking to their desk. You can also get daily assignments by walking to the file cabinet. Homeroom is the perfect solution to create an interactive and focused classroom environment for students to learn in the best way possible.", style: TextStyle(color: currTextColor, fontWeight: FontWeight.normal, fontSize: 20),),
+                        ],
+                      ),
                     ),
                     new Padding(padding: EdgeInsets.all(32)),
                     new RaisedButton(
