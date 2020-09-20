f6313449932d7dcd372dcab6d42324b2cd69e830
Linus Zheng <linus.zheng@warriorlife.net>
2020-09-20 00:15:17 -0700

    got rid of conflicts

--- web/script.js
+++ web/script.js
@@ -178,28 +178,6 @@ function updatePositionListener(id) {
 // var difX, difY;
 
 localStreamDiv = addStream('my-stream');
-<<<<<<< HEAD
-localStreamDiv.setAttribute('draggable','true');
-localStreamDiv.addEventListener("dragstart", function(ev){
-    difX = ev.clientX - localStreamDiv.getBoundingClientRect().left;
-    difY = ev.clientY - localStreamDiv.getBoundingClientRect().top;
-});
-remoteContainer.addEventListener("dragover", function(ev){
-    ev.preventDefault();
-});
-remoteContainer.addEventListener("drop", function(ev){
-    ev.preventDefault();
-    var newX = ev.clientX - difX - remoteContainer.getBoundingClientRect().left;
-    var newY = ev.clientY - difY - remoteContainer.getBoundingClientRect().top; 
-    newX = Math.round(newX/unitSize)*unitSize;
-    newY = Math.round(newY/unitSize)*unitSize;
-    localStreamDiv.style.left = newX+'px';
-    localStreamDiv.style.top = newY+'px';
-    db.ref('rooms/'+options.channel+'/users').child(String(localStream.getId())).update({
-        x: newX, y: newY,
-    });
-});
-=======
 localStreamDiv.setAttribute('draggable','false');
 // localStreamDiv.addEventListener("dragstart", function(ev){
 //     difX = ev.clientX - localStreamDiv.getBoundingClientRect().left;
@@ -220,4 +198,3 @@ localStreamDiv.setAttribute('draggable','false');
 //         x: newX, y: newY,
 //     });
 // });
->>>>>>> 04e4c3e47b52d8d315cb1c8bab0468b7e42ed0ac
